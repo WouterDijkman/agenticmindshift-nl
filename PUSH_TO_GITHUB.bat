8d5f9619c982584d65afcb0e91b071d82171a843
FaberWO <wwdijkman@hotmail.com>
2026-05-26 13:14:38 +0200

    feat: Factum Capital reframe + scorecard redesign voor 4 momenten

--- PUSH_TO_GITHUB.bat
+++ PUSH_TO_GITHUB.bat
@@ -6,44 +6,34 @@ echo.
 
 cd /d "%~dp0"
 
-echo [1/6] Verwijder git locks...
-if exist ".git\index.lock" del /f ".git\index.lock" && echo       index.lock verwijderd.
-if exist ".git\config.lock" del /f ".git\config.lock" && echo       config.lock verwijderd.
+echo [1/4] Verwijder git locks...
+if exist ".git\index.lock" del /f ".git\index.lock"
+if exist ".git\config.lock" del /f ".git\config.lock"
 
 echo.
-echo [2/6] Remote instellen...
+echo [2/4] Remote instellen...
 git remote remove origin 2>nul
 git remote add origin https://github.com/WouterDijkman/agenticmindshift-nl.git
 echo       Remote: https://github.com/WouterDijkman/agenticmindshift-nl.git
 
 echo.
-echo [3/6] Stage alle wijzigingen...
+echo [3/4] Stage + commit (als er iets nieuws is)...
 git add -A
-if %errorlevel% neq 0 (
-    echo FOUT bij git add.
-    pause
-    exit /b 1
-)
+git commit -m "feat: Factum Capital reframe + scorecard redesign voor 4 momenten" 2>nul
+echo       (niets te committen = al gedaan, doorgaan naar push)
 
 echo.
-echo [4/6] Commit...
-git commit -m "feat: Factum Capital reframe + scorecard redesign voor 4 momenten"
-if %errorlevel% neq 0 (
-    echo Niets te committen, of commit mislukt.
-    pause
-    exit /b 1
-)
-
-echo.
-echo [5/6] Push naar GitHub...
+echo [4/4] Push naar GitHub...
 git push -u origin master
 if %errorlevel% neq 0 (
-    echo Probeer ook: git push -u origin main
+    echo.
+    echo Probeer ook main branch...
     git push -u origin main
 )
 
 echo.
-echo [6/6] Klaar! Alles staat op GitHub.
-echo https://github.com/WouterDijkman/agenticmindshift-nl
+echo ============================================
+echo  Klaar! Check: https://github.com/WouterDijkman/agenticmindshift-nl
+echo ============================================
 echo.
 pause
